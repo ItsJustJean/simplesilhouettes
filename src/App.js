3c8864ac61d2db7ceebba5ff3ec71bf5fb04f1ca
ItsJustJean <itsjean2002@gmail.com>
2022-09-15 14:20:32 -0400

    Personal Tinkering Website
    Plans to expand this basic HTML website into a fully fledged REACT and JS site. Theme is going to revolve around fashion, however i might include fun little programs to keep the user entertained.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <h1>simplesilhouettes</h1>
+       <h2>Just a barebones page at the moment, this is all placeholder for the larger website to come.</h2>
+       <img src="IMG_7837.jpg" alt ="Jean Profile" width = "10%" ></img>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+       <hr></hr>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
